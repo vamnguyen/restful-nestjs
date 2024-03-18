e2254475e45159e210c64fb67548042262966757
Minh Nguyen <anhminh.dev@gmail.com>
2024-03-18 15:27:10 +0700

    Add relational db schema and config script package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,17 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { NoteModule } from './note/note.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, NoteModule, PrismaModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    NoteModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,63 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDTO } from './dto';
+import * as argon from 'argon2';
+
 @Injectable({}) // This is dependency injection
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
 
-  register() {
-    return {
-      message: 'Register new user',
-    };
+  async register(authDTO: AuthDTO) {
+    // Hash the password
+    const hashedPassword = await argon.hash(authDTO.password);
+    try {
+      // Save the user to the database
+      const user = await this.prismaService.user.create({
+        data: {
+          email: authDTO.email,
+          hashedPassword,
+          firstName: '',
+          lastName: '',
+        },
+        // Only return the id, email, firstName, lastName and createdAt of the user
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+          createdAt: true,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Email already exists');
+      }
+    }
   }
 
-  login() {
-    return 'Login...';
+  async login(authDTO: AuthDTO) {
+    // find user by email
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email: authDTO.email,
+      },
+    });
+
+    // If the user is not found, throw an error
+    if (!user) {
+      throw new ForbiddenException('User not found');
+    }
+
+    // Compare the password
+    const isPasswordValid = await argon.verify(
+      user.hashedPassword,
+      authDTO.password,
+    );
+    if (!isPasswordValid) {
+      throw new ForbiddenException('Invalid password');
+    }
+
+    return user;
   }
 }
