3cbef1b5f7059ee6b33c4c5986afc8f331f478f7
Minh Nguyen <anhminh.dev@gmail.com>
2024-03-24 14:30:17 +0700

    feat: JWT Guards to check token of req and custom decorator

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JWTStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    configService: ConfigService,
+    public prismService: PrismaService,
+  ) {
+    super({
+      // token string is added to the request header, to every request (except login and register)
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      // secret key is used to verify the token
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.prismService.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+    delete user.hashedPassword;
+    return JSON.stringify(user);
+  }
+}
