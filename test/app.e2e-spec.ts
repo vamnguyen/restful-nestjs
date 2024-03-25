8fd65bda10583c57eb11a5559829a45aaf1beaef
Minh Nguyen <anhminh.dev@gmail.com>
2024-03-25 07:28:32 +0700

    write test case for authentication and user info

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -8,6 +8,7 @@ import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from '../src/prisma/prisma.service';
+import * as pactum from 'pactum';
 
 const PORT = 3002; // This is PORT for testing purpose only.
 
@@ -27,6 +28,92 @@ describe('App End2End tests', () => {
 
     prismaService = app.get(PrismaService);
     await prismaService.cleanDatabase();
+
+    pactum.request.setBaseUrl(`http://localhost:${PORT}`);
+  });
+
+  describe('Test Authentication', () => {
+    describe('Register', () => {
+      it('should Register', () => {
+        return pactum
+          .spec()
+          .post(`/auth/register`)
+          .withBody({
+            email: 'abc123@gmail.com',
+            password: 'abc123',
+            firstName: 'Minh',
+            lastName: 'Nguyen',
+          })
+          .expectStatus(201);
+        // .inspect();
+      });
+
+      it('should show error with empty email', () => {
+        return pactum
+          .spec()
+          .post(`/auth/register`)
+          .withBody({
+            email: '',
+            password: 'abc123',
+            firstName: 'Minh',
+            lastName: 'Nguyen',
+          })
+          .expectStatus(400);
+      });
+
+      it('should show error with invalid email format', () => {
+        return pactum
+          .spec()
+          .post(`/auth/register`)
+          .withBody({
+            email: 'minhgmail',
+            password: 'abc123',
+            firstName: 'Minh',
+            lastName: 'Nguyen',
+          })
+          .expectStatus(400);
+      });
+    });
+
+    describe('Login', () => {
+      it('should Login', () => {
+        return (
+          pactum
+            .spec()
+            .post(`/auth/login`)
+            .withBody({
+              email: 'abc123@gmail.com',
+              password: 'abc123',
+            })
+            .expectStatus(201)
+            // .inspect()
+            .stores('accessToken', 'accessToken')
+        );
+      });
+
+      it('should show error if empty password', () => {
+        return pactum
+          .spec()
+          .post(`/auth/login`)
+          .withBody({
+            email: 'abc123@gmail.com',
+            password: '',
+          })
+          .expectStatus(400);
+      });
+    });
+
+    describe('User Info', () => {
+      it('should get user info', () => {
+        return pactum
+          .spec()
+          .get(`/users/me`)
+          .withHeaders({
+            Authorization: `Bearer $S{accessToken}`,
+          })
+          .expectStatus(200);
+      });
+    });
   });
 
   afterAll(async () => {
